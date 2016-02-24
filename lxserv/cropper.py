67513c076d8061f3c662f76bda6fa171bbc846b7
adamohern <adam@evd1.tv>
2016-02-24 09:15:38 -0500

    WORKING!

--- lxserv/cropper.py
+++ lxserv/cropper.py
@@ -25,10 +25,10 @@ def quick_user_value(value_handle, value_type='string', nicename='', default='')
 
 def get_target_frame():
     render_region = {
-        'left': float(modo.Scene().renderItem.channel('regX0').get()),
-        'right': float(modo.Scene().renderItem.channel('regX1').get()),
-        'top': float(modo.Scene().renderItem.channel('regY0').get()),
-        'bottom': float(modo.Scene().renderItem.channel('regY1').get())
+        'left': modo.Scene().renderItem.channel('regX0').get(),
+        'right': modo.Scene().renderItem.channel('regX1').get(),
+        'top': modo.Scene().renderItem.channel('regY0').get(),
+        'bottom': modo.Scene().renderItem.channel('regY1').get()
     }
 
     return [
@@ -44,15 +44,15 @@ def get_target_offset(new_aperture):
     ]
 
     offset = [
-        float(modo.Scene().renderCamera.channel('offsetX').get()),
-        float(modo.Scene().renderCamera.channel('offsetY').get())
+        modo.Scene().renderCamera.channel('offsetX').get(),
+        modo.Scene().renderCamera.channel('offsetY').get()
     ]
 
     render_region = {
-        'left': float(modo.Scene().renderItem.channel('regX0').get()),
-        'right': float(modo.Scene().renderItem.channel('regX1').get()),
-        'top': float(modo.Scene().renderItem.channel('regY0').get()),
-        'bottom': float(modo.Scene().renderItem.channel('regY1').get())
+        'left': modo.Scene().renderItem.channel('regX0').get(),
+        'right': modo.Scene().renderItem.channel('regX1').get(),
+        'top': modo.Scene().renderItem.channel('regY0').get(),
+        'bottom': modo.Scene().renderItem.channel('regY1').get()
     }
 
     x_step = 1
@@ -117,7 +117,7 @@ def get_latest_pass():
     return max(passes, key=lambda p: p.index)
 
 
-def get_target_aperture():
+def get_proportional_aperture():
     frame = [
         int(modo.Scene().renderItem.channel('resX').get()),
         int(modo.Scene().renderItem.channel('resY').get())
@@ -141,9 +141,49 @@ def get_target_aperture():
     return aperture
 
 
+def get_target_aperture(target_frame, proportional_aperture):
+    render_region = {
+        'left': modo.Scene().renderItem.channel('regX0').get(),
+        'right': modo.Scene().renderItem.channel('regX1').get(),
+        'top': modo.Scene().renderItem.channel('regY0').get(),
+        'bottom': modo.Scene().renderItem.channel('regY1').get()
+    }
+
+    region_size = [
+        render_region['right'] - render_region['left'],
+        render_region['bottom'] - render_region['top']
+    ]
+
+    if target_frame[0] >= target_frame[1]:
+
+        if target_frame[0] > target_frame[1]:
+            target_aperture = [
+                proportional_aperture[0] * region_size[0],
+                modo.Scene().renderCamera.channel('apertureY').get()
+            ]
+
+        if target_frame[0] == target_frame[1]:
+            target_aperture = [
+                proportional_aperture[1] * region_size[0],
+                modo.Scene().renderCamera.channel('apertureY').get()
+            ]
+
+        if target_frame[0] < target_frame[1]:
+            target_aperture = [
+                modo.Scene().renderCamera.channel('apertureX').get(),
+                proportional_aperture[1] * region_size[1]
+            ]
+    else:
+        target_aperture = [
+            modo.Scene().renderCamera.channel('apertureX').get(),
+            proportional_aperture[1] * region_size[1]
+        ]
+
+    return target_aperture
+
 
 class mecco_cropper(lxu.command.BasicCommand):
-    
+
     def __init__(self):
         lxu.command.BasicCommand.__init__(self)
         self.dyna_Add('name', lx.symbol.sTYPE_STRING)
@@ -154,10 +194,10 @@ class mecco_cropper(lxu.command.BasicCommand):
         if not pass_name:
             pass_name = DEFAULT_PASSNAME
 
-        target_aperture = get_target_aperture()
+        proportional_aperture = get_proportional_aperture()
         target_frame = get_target_frame()
-        target_offset = get_target_offset(target_aperture)
-        long_axis = 'X' if target_frame[0] >= target_frame[1] else 'Y'
+        target_offset = get_target_offset(proportional_aperture)
+        target_aperture = get_target_aperture(target_frame, proportional_aperture)
 
         modo.Scene().renderItem.channel('region').set(False)
 
@@ -171,7 +211,8 @@ class mecco_cropper(lxu.command.BasicCommand):
             modo.Scene().renderItem.channel('resY'),
             modo.Scene().renderCamera.channel('offsetX'),
             modo.Scene().renderCamera.channel('offsetY'),
-            modo.Scene().renderCamera.channel('aperture{}'.format(long_axis))
+            modo.Scene().renderCamera.channel('apertureX'),
+            modo.Scene().renderCamera.channel('apertureY')
         ]
 
         for channel in channels_list:
@@ -184,7 +225,8 @@ class mecco_cropper(lxu.command.BasicCommand):
         modo.Scene().renderItem.channel('resY').set(target_frame[1]),
         modo.Scene().renderCamera.channel('offsetX').set(target_offset[0]),
         modo.Scene().renderCamera.channel('offsetY').set(target_offset[1]),
-        modo.Scene().renderCamera.channel('aperture{}'.format(long_axis)).set(target_offset[1])
+        modo.Scene().renderCamera.channel('apertureX').set(target_aperture[0]),
+        modo.Scene().renderCamera.channel('apertureY').set(target_aperture[1])
 
         lx.eval('edit.apply')
 
@@ -206,6 +248,6 @@ class mecco_cropper_toggle(lxu.command.BasicCommand):
                 activate_latest_pass()
             except:
                 pass
-        
+
 lx.bless(mecco_cropper_toggle, "mecco.cropperToggle")
 lx.bless(mecco_cropper, "mecco.cropper")
