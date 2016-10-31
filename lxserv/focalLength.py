3ec4c7d123994ff98f1de84fe421b4f9eac8d1e0
adamohern <adam@ChocolateChip.local>
2016-10-31 10:17:05 -0400

    focalLength fix, todo items

--- lxserv/focalLength.py
+++ lxserv/focalLength.py
@@ -6,6 +6,8 @@ class commandClass(lxu.command.BasicCommand):
     def basic_Execute(self, msg, flags):
         cameraName = modo.Scene().renderCamera.id
 
+        lx.eval('select.drop item')
+        lx.eval('select.drop channel')
         modo.Scene().renderCamera.select()
         lx.eval('select.channel {%s:focalLen} add' % cameraName)
         lx.eval('tool.set channel.haul on')
